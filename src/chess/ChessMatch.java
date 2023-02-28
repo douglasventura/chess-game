9f9e3a62b0f5b588156f15d14e43798d13f1e6f7
Douglas Ventura <douglasventura.pe@gmail.com>
2023-02-28 14:52:06 -0300

    Bishop logic

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -7,6 +7,7 @@ import java.util.stream.Collectors;
 import boardgame.Board;
 import boardgame.Piece;
 import boardgame.Position;
+import chess.pieces.Bishop;
 import chess.pieces.King;
 import chess.pieces.Pawn;
 import chess.pieces.Rook;
@@ -198,7 +199,7 @@ public class ChessMatch {
 
   private void initialSetup() {
 
-    // create white pieces
+    // WHITE PIECES
     // pawns
     placeNewPiece('a', 2, new Pawn(board, Color.WHITE));
     placeNewPiece('b', 2, new Pawn(board, Color.WHITE));
@@ -209,12 +210,18 @@ public class ChessMatch {
     placeNewPiece('g', 2, new Pawn(board, Color.WHITE));
     placeNewPiece('h', 2, new Pawn(board, Color.WHITE));
 
+    // rooks
     placeNewPiece('a', 1, new Rook(board, Color.WHITE));
-    placeNewPiece('e', 1, new Rook(board, Color.WHITE));
+    placeNewPiece('h', 1, new Rook(board, Color.WHITE));
 
+    // bishops
+    placeNewPiece('c', 1, new Bishop(board, Color.WHITE));
+    placeNewPiece('f', 1, new Bishop(board, Color.WHITE));
+
+    // another
     placeNewPiece('d', 1, new King(board, Color.WHITE));
 
-    // create black pieces
+    // BLACK PIECES
     // pawns
     placeNewPiece('a', 7, new Pawn(board, Color.BLACK));
     placeNewPiece('b', 7, new Pawn(board, Color.BLACK));
@@ -225,9 +232,15 @@ public class ChessMatch {
     placeNewPiece('g', 7, new Pawn(board, Color.BLACK));
     placeNewPiece('h', 7, new Pawn(board, Color.BLACK));
 
+    // rooks
     placeNewPiece('a', 8, new Rook(board, Color.BLACK));
-    placeNewPiece('e', 8, new Rook(board, Color.BLACK));
+    placeNewPiece('h', 8, new Rook(board, Color.BLACK));
+    
+    // bishops
+    placeNewPiece('c', 8, new Bishop(board, Color.BLACK));
+    placeNewPiece('f', 8, new Bishop(board, Color.BLACK));
 
+    // another
     placeNewPiece('d', 8, new King(board, Color.BLACK));
   }
 
---
+++ src/chess/pieces/Bishop.java
@@ -0,0 +1,73 @@
+package chess.pieces;
+
+import boardgame.Board;
+import boardgame.Position;
+import chess.ChessPiece;
+import chess.Color;
+
+public class Bishop extends ChessPiece {
+
+  public Bishop(Board board, Color color) {
+    super(board, color);
+  }
+
+  @Override
+  public String toString() {
+    return "B";
+  }
+
+  @Override
+  public boolean[][] possibleMoves() {
+    boolean[][] matrix = new boolean[getBoard().getRows()][getBoard().getColumns()];
+
+    Position p = new Position(0, 0);
+
+    // check possible moves
+    // northeast
+    p.setValues(position.getRow() + 1, position.getColumn() + 1);
+    while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+      matrix[p.getRow()][p.getColumn()] = true;
+      p.setValues(p.getRow() + 1, p.getColumn() + 1);
+    }
+
+    if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+      matrix[p.getRow()][p.getColumn()] = true;
+    }
+
+    // northwest
+    p.setValues(position.getRow() + 1, position.getColumn() - 1);
+    while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+      matrix[p.getRow()][p.getColumn()] = true;
+      p.setValues(p.getRow() + 1, p.getColumn() - 1);
+    }
+
+    if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+      matrix[p.getRow()][p.getColumn()] = true;
+    }
+
+    // southeast
+    p.setValues(position.getRow() - 1, position.getColumn() + 1);
+    while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+      matrix[p.getRow()][p.getColumn()] = true;
+      p.setValues(p.getRow() - 1, p.getColumn() + 1);
+    }
+
+    if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+      matrix[p.getRow()][p.getColumn()] = true;
+    }
+
+    // southwest
+    p.setValues(position.getRow() - 1, position.getColumn() - 1);
+    while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+      matrix[p.getRow()][p.getColumn()] = true;
+      p.setValues(p.getRow() - 1, p.getColumn() - 1);
+    }
+
+    if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+      matrix[p.getRow()][p.getColumn()] = true;
+    }
+
+    return matrix;
+  }
+   
+}
